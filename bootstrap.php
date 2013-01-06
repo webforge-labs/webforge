b482aaeb9fb41abe2d00c47de589c8d74c3c32f5
Philipp Scheit <p.scheit@ps-webforge.com>
2013-01-06 10:20:13 +0100

    adjust bootstrap for new psc-cms

--- bootstrap.php
+++ bootstrap.php
@@ -14,7 +14,7 @@ if (file_exists($cmsBootstrap = getenv('PSC_CMS').'bootstrap.php')) {
 }
 
 if (!class_exists('Psc\PSC', FALSE)) {
-  require_once __DIR__.$ds.'vendor'.$ds.'pscheit'.$ds.'psc-cms'.$ds.'bin'.$ds.'psc-cms.phar.gz';
+  require_once __DIR__.$ds.'vendor'.$ds.'bin'.$ds.'psc-cms.phar.gz';
 }
 
 return $GLOBALS['env']['root'] = new \Psc\System\Dir(__DIR__.DIRECTORY_SEPARATOR);
