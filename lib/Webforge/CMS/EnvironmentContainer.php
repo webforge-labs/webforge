dcc0cb2ef1ecd74b6178c7338920174830996a1d
Philipp Scheit <p.scheit@ps-webforge.com>
2013-06-08 12:11:41 +0200

    add an EnvironmentContainer for Klasses that are tightly coupled to PHP behaviour from the environment

---
+++ lib/Webforge/CMS/EnvironmentContainer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Webforge\CMS;
+
+use Psc\Session\Session;
+use Psc\PHP\CookieManager;
+
+class EnvironmentContainer {
+
+  protected $session;
+  protected $cookieManager;
+
+  protected $options = array();
+
+  public function __construct(Array $options = array()) {
+    $this->options = array_replace(
+      array(
+        'cookies.domain' => @$_SERVER['HTTP_HOST'],
+        'session.init' => TRUE
+      ),
+      $options
+    );
+  }
+
+  public function getSession() {
+    if (!isset($this->session)) {
+      $this->session = new Session();
+
+      if ($this->getOption('session.init')) {
+        $this->session->init();
+      }
+    }
+
+    return $this->session;
+  }
+
+  public function setSession(Session $session) {
+    $this->session = $session;
+    return $this;
+  }
+
+  public function getCookieManager() {
+    if (!isset($this->cookieManager)) {
+      $this->cookieManager = new CookieManager();
+      $this->cookieManager->setDomain($this->getOption('cookies.domain'));
+
+    }
+    return $this->cookieManager;
+  }
+
+  public function setCookieManager(CookieManager $cookieManager) {
+    $this->cookieManager = $cookieManager;
+    return $this;
+  }
+
+  public function getOption($key) {
+    if (array_key_exists($key, $this->options)) {
+      return $this->options[$key];
+    }
+
+    return NULL;
+  }
+
+  public function setOption($key, $value) {
+    $this->options[$key] = $value;
+  }
+}
---
+++ tests/Webforge/CMS/EnvironmentContainerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Webforge\CMS;
+
+class EnvironmentContainerTest extends \Webforge\Code\Test\Base {
+  
+  public function setUp() {
+    $this->chainClass = 'Webforge\\CMS\\EnvironmentContainer';
+    parent::setUp();
+
+    $this->container = new EnvironmentContainer(array('session.init'=>FALSE));
+  }
+
+  /**
+   * @dataProvider provideDependencies
+   */
+  public function testHasAllRequiredDependencies($name, $fqn) {
+    $this->assertInstanceOf($fqn, $this->container->{'get'.$name}());
+  }
+
+  public function testWritesCookieDomainOption() {
+    $this->assertEquals('', $this->container->getOption('cookies.domain'));
+
+    $this->container->setOption('cookies.domain', 'my.domain.com');
+
+    $this->assertEquals('my.domain.com', $this->container->getOption('cookies.domain'));
+  }
+
+  /**
+   * @dataProvider provideDependencies
+   */
+  public function testInjectsAllDependencies($name, $fqn) {
+    $mock = $this->getMockBuilder($fqn)->disableOriginalConstructor()->getMock();
+
+    $this->assertChainable($this->container->{'set'.$name}($mock));
+
+    $this->assertSame(
+      $mock,
+      $this->container->{'get'.$name}()
+    );
+  }
+
+  public static function provideDependencies() {
+    $tests = array();
+  
+    $test = function() use (&$tests) {
+      $tests[] = func_get_args();
+    };
+  
+    $test('Session', 'Psc\Session\Session');
+    $test('CookieManager', 'Psc\PHP\CookieManager');
+  
+    return $tests;
+  }
+}
