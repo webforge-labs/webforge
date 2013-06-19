f58802d04449e0b715c8dd8145fd9e250daa1756
Philipp Scheit <p.scheit@ps-webforge.com>
2013-06-19 12:56:47 +0200

    fix closure for 5.3

--- lib/Webforge/Setup/Installer/CMSContainerPart.php
+++ lib/Webforge/Setup/Installer/CMSContainerPart.php
@@ -13,14 +13,16 @@ class CMSContainerPart extends Part implements PackageAware {
   }
   
   public function installTo(Dir $target, Installer $installer) {
+    $namespace = $this->package->getNamespace();
+
     $installer->createClass('CMS\Container', Installer::IF_NOT_EXISTS)
       ->parent('Psc\CMS\AbstractContainer')
-      ->withGClass(function ($gClass) use ($installer) {
+      ->withGClass(function ($gClass) use ($installer, $namespace) {
         $gClass->addProperty(
           GProperty::create(
             'defaultNamespace', 
             'String', 
-            $installer->ask('Specify the default ControllerNamespace:', $this->package->getNamespace().'\\Controllers')
+            $installer->ask('Specify the default ControllerNamespace:', $namespace.'\\Controllers')
           )
         );
       })
