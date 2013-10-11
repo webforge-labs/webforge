2895f457e3f5cea339120304dacaefd2cae5c464
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-11 09:00:13 +0200

    add a boot container that simply bootstraps the webforge container from the package

---
+++ lib/Webforge/Setup/BootContainer.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Webforge\Setup;
+
+use Webforge\Framework\Container as WebforgeContainer;
+use Webforge\Framework\LocalPackageInitException;
+use Webforge\Common\System\Dir;
+
+class BootContainer {
+
+  public $webforge;
+
+  protected $rootDirectory;
+
+  public function __construct($rootDirectory) {
+    $this->webforge = new WebforgeContainer();
+    $this->initRootDirectory($rootDirectory);
+  }
+
+  public function init() {
+
+  }
+
+  public function getWebforge() {
+    return $this->webforge;
+  }
+
+  protected function initRootDirectory($rootDirectory) {
+    if ($rootDirectory instanceof Dir) {
+      $this->rootDirectory = $rootDirectory;
+    } else {
+      $this->rootDirectory = Dir::factoryTS($rootDirectory);
+    }
+  }
+
+  /**
+   * Tries to init the package with webforge automatically
+   *
+   * this can fail in some cases:
+   *   1. the easiest way is to webforge register-package the package which should be bootstrapped
+   *   2. the next way would be to provide a composer.json with autoload infos in the rootDirectory of the container
+   *   3. some older projects can have their composer.json in root\Umsetzung\base\src but this is discouraged to use and can be removed in the future
+   *
+   * after this is called the local package should be registered in webforge container (getLocalPackage() / getLocalProject())
+   */
+  public function initLocalWebforgePackage() {
+    try {
+      $this->webforge->initLocalPackageFromDirectory($this->rootDirectory);
+    } catch (LocalPackageInitException $e) {
+      // this could happen for packages that are not (yet) registered by webforge
+      // but thats not a problem at first hand: we assume that we can find a composer.json somewhere
+      
+      $this->webforge->getPackageRegistry()->addComposerPackageFromDirectory(
+        $this->rootDirectory
+      );
+      
+      // try again to init (its faster to use $composerRoot here, allthough this->rootDirectory would do it anyway)
+      $this->webforge->initLocalPackageFromDirectory($this->rootDirectory); 
+    }  
+  }
+  
+  /**
+   * @return Webforge\Configuration\Configuration
+   */
+  public function getHostConfiguration() {
+    return $this->webforge->getCMSBridge()->getHostConfig();
+  }
+
+  /**
+   * @return Webforge\Configuration\Configuration
+   */
+  public function getHostConfig() {
+    return $this->getHostConfiguration();
+  }
+}
---
+++ tests/Webforge/Setup/BootContainerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Webforge\Setup;
+
+class BootContainerTest extends \Webforge\Code\Test\Base {
+  
+  public function setUp() {
+    $this->chainClass = __NAMESPACE__ . '\\BootContainer';
+    parent::setUp();
+
+    require_once $this->getPackageDir('vendor/pscheit/psc-cms-boot/lib/')->getFile('package.boot.php');
+    $this->boot = new \Psc\Boot\BootLoader((string) $GLOBALS['env']['root'], $this->chainClass);
+
+    $this->container = $this->boot->getContainer();
+  }
+
+  public function testBootConstructsTheContainer() {
+    $this->assertInstanceOf($this->chainClass, $this->boot->getContainer());
+  }
+
+  public function testReturnsAWebforgeContainer() {
+    $this->assertInstanceOf('Webforge\Framework\Container', $this->container->getWebforge());
+  }
+
+  public function testInitsTheLocalPackageFromContainer() {
+    $this->container->initLocalWebforgePackage();
+
+    $this->assertInstanceOf('Webforge\Framework\Package\Package', $package = $this->container->getWebforge()->getLocalPackage());
+    $this->assertEquals('webforge/webforge', $package->getIdentifier());
+  }
+
+  public function testReturnsTheHostConfig() {
+    $this->assertInstanceOf('Webforge\Configuration\Configuration', $this->container->getHostConfiguration());
+    $this->assertInstanceOf('Webforge\Configuration\Configuration', $this->container->getHostConfig());
+  }
+}
