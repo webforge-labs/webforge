3869d14a6106153c9e92ab4166857f7a16cd6992
Philipp Scheit <p.scheit@ps-webforge.com>
2017-11-14 23:00:26 +0100

    remove coveralls

--- lib/inc.commands.php
+++ lib/inc.commands.php
@@ -10,7 +10,6 @@ use Webforge\Framework\Package\Package;
 use Webforge\Common\JS\JSONConverter;
 use Webforge\Common\System\File;
 use Webforge\Common\System\Dir;
-use Webforge\Common\StringUtil;
 use Webforge\Common\CLassUtil;
 use Webforge\Console\SymfonyCommandOutputAdapter;
 use Webforge\Console\InteractionHelper;
